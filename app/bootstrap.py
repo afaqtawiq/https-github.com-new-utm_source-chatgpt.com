dd7c0f452845acb3dab091567d4966a6baf9c622
afaqtawiq <sona.aqo@gmail.com>
2026-09-08 17:19:09 +0300

    Register customer success retention engine

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -16,6 +16,7 @@ from app.operations_control import router as operations_control_router
 from app.control_tower import router as control_tower_router
 from app.ceo_command import router as ceo_command_router
 from app.customer360 import router as customer360_router
+from app.customer_success import router as customer_success_router
 
 app.include_router(verification_router)
 app.include_router(intelligence_router)
@@ -34,3 +35,4 @@ app.include_router(operations_control_router)
 app.include_router(control_tower_router)
 app.include_router(ceo_command_router)
 app.include_router(customer360_router)
+app.include_router(customer_success_router)
