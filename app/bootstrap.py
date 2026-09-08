955dd99a59ee3b229a77a3dcedf736754b3bd480
afaqtawiq <sona.aqo@gmail.com>
2026-09-08 17:07:51 +0300

    Register operations control tower

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -13,6 +13,7 @@ from app.quote_builder import router as quote_builder_router
 from app.quote_pricing import router as quote_pricing_router
 from app.quote_workflow import router as quote_workflow_router
 from app.operations_control import router as operations_control_router
+from app.control_tower import router as control_tower_router
 
 app.include_router(verification_router)
 app.include_router(intelligence_router)
@@ -28,3 +29,4 @@ app.include_router(quote_builder_router)
 app.include_router(quote_pricing_router)
 app.include_router(quote_workflow_router)
 app.include_router(operations_control_router)
+app.include_router(control_tower_router)
