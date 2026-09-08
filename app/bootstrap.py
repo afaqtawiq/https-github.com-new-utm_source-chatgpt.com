89175a74db26652742d2c698cd5a9682495e6967
afaqtawiq <sona.aqo@gmail.com>
2026-09-08 17:11:32 +0300

    Register CEO command center

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -14,6 +14,7 @@ from app.quote_pricing import router as quote_pricing_router
 from app.quote_workflow import router as quote_workflow_router
 from app.operations_control import router as operations_control_router
 from app.control_tower import router as control_tower_router
+from app.ceo_command import router as ceo_command_router
 
 app.include_router(verification_router)
 app.include_router(intelligence_router)
@@ -30,3 +31,4 @@ app.include_router(quote_pricing_router)
 app.include_router(quote_workflow_router)
 app.include_router(operations_control_router)
 app.include_router(control_tower_router)
+app.include_router(ceo_command_router)
